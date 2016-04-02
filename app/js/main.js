04b7a7b2ef59bf5ab8734aec9032877a14a5c9dc
Isaac Sanchez <isaacsanchez@MacBook-Air-de-Isaac.local>
2016-04-02 16:50:02 +0200

    dinamic page slider whith polymer and firebase

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+(function(document) {
+	'use strict';
+
+	var app = document.querySelector('#app');
+ 	app.baseUrl = '/';
+ 	
+  	if (window.location.port === '') {  // if production
+    	// Uncomment app.baseURL below and
+    	// set app.baseURL to '/your-pathname/' if running from folder in production
+    	app.baseUrl = '/cells/cellsjs/index.html';
+  	}
+
+})(document);
