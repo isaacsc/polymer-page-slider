a68f579e01c90f9fa2f691dad58404973e270b49
Isaac Sanchez <isaacsanchez@MacBook-Air-de-Isaac.local>
2016-04-02 19:55:55 +0200

    express server added whith redirection to index

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/app'));
+
+app.get('/',function(request, response){
+  cresponse.sendFile(__dirname + '/app/index.html');
+});
+
+app.get('*', function(request, response){
+  response.sendFile(__dirname + '/app/index.html');
+});
+
+app.listen(3000, function(){
+  console.log('Server running at http://127.0.0.1:3000/');
+});
