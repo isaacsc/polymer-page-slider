6fc0f43d2f1d0f3f6d4821f18d0f0629956bea09
Isaac Sanchez <isaacsanchez@MacBook-Air-de-Isaac.local>
2016-04-06 12:03:53 +0200

    server test

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
 
-app.get('*', function(request, response){
-  response.sendFile(__dirname + '/app/index.html');
+app.get('/:page', function(request, response){
+	console.log('parametro',request.params.page);
+  	response.sendFile(__dirname + '/app/index.html');
+});
+
+app.get('/', function(request, response){
+	console.log('vacio');
+  	response.sendFile(__dirname + '/app/index.html');
 });
 
 app.listen(3000, function(){
-  console.log('Server running at http://127.0.0.1:3000/');
+  	console.log('Server running at http://127.0.0.1:3000/');
 });
