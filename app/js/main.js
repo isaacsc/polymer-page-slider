10f6c37e4f8be909af53659ef7310eb9b1081f87
Isaac Sanchez <isaacsanchez@MacBook-Air-de-Isaac.local>
2016-04-07 21:45:08 +0200

    order code

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@
   	if (window.location.port === '') {  // if production
     	// Uncomment app.baseURL below and
     	// set app.baseURL to '/your-pathname/' if running from folder in production
-    	app.baseUrl = '/cells/cellsjs/index.html';
+    	app.baseUrl = '/';
   	}
 
 })(document);
